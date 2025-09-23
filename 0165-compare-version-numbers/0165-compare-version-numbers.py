fcb709ad83b3cee356274f32f25cabc2054eda1f
Soyeon Kim <iscowkite@gmail.com>
2025-09-23 22:21:53 +0900

    Time: 0 ms (100%), Space: 17.8 MB (60.47%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -1,24 +1,13 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        v1_numbers = version1.split('.')
-        v2_numbers = version2.split('.')
+        a = [int(x) for x in version1.split('.')]
+        b = [int(x) for x in version2.split('.')]
 
-        compareable_unit = min(len(v1_numbers), len(v2_numbers))
-        for i in range(compareable_unit):
-            if int(v1_numbers[i]) < int(v2_numbers[i]):
+        for x, y in zip_longest(a, b, fillvalue=0):
+            if x < y:
                 return -1
-            elif int(v1_numbers[i]) > int(v2_numbers[i]):
+            if x > y:
                 return 1
-
-        if len(v1_numbers) == len(v2_numbers):
-            return 0
-
-        v1_rest = "".join(v1_numbers[compareable_unit:])
-        v2_rest = "".join(v2_numbers[compareable_unit:])
-
-        v1_rest_int = int(v1_rest) if v1_rest else 0
-        v2_rest_int = int(v2_rest) if v2_rest else 0
-
-        return 1 if v1_rest_int > 0 else -1 if v2_rest_int > 0 else 0
+        return 0
 
         
